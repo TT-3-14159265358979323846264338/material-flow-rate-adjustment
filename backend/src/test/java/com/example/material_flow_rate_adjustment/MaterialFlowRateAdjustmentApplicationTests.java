a5e85c97c80fabaea42ec6e51c7c499076b92b73
T.T. <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2026-05-12 08:11:40 +0900

    Add files via upload

---
+++ backend/src/test/java/com/example/material_flow_rate_adjustment/MaterialFlowRateAdjustmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.material_flow_rate_adjustment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialFlowRateAdjustmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

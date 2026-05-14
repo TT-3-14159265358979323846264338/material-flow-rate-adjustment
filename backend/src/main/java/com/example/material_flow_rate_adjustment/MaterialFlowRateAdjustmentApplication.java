f7525ecd98894ad1e84ec88021d70a5b5095a122
T.T. <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2026-05-15 06:58:45 +0900

    Add files via upload

--- backend/src/main/java/com/example/material_flow_rate_adjustment/MaterialFlowRateAdjustmentApplication.java
+++ backend/src/main/java/com/example/material_flow_rate_adjustment/MaterialFlowRateAdjustmentApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MaterialFlowRateAdjustmentApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MaterialFlowRateAdjustmentApplication.class, args);
 	}
-
 }
---
+++ backend/src/main/java/com/example/material_flow_rate_adjustment/savedata/MaterialRepository.java
@@ -0,0 +1,8 @@
+package com.example.material_flow_rate_adjustment.savedata;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaterialRepository extends JpaRepository<MaterialSQL, Integer>{
+}

59f921c5f7aae2bea5412fb146de9a481e1ea82d
T.T. <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2026-05-26 06:56:27 +0900

    Delete backend/src/main/java/com/example/material_flow_rate_adjustment/savedata directory

--- backend/src/main/java/com/example/material_flow_rate_adjustment/savedata/AdjustmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.material_flow_rate_adjustment.savedata;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdjustmentRepository extends JpaRepository<AdjustmentSQL, Integer>{
-}
